b12221429d7775d4c27ec4ea64b6d05be95ab7d9
m-abdelkawy <engineer_m.abdelkawy@yahoo.com>
2021-12-13 23:23:44 -0600

    Initial Commit

---
+++ API/Controllers/ProductsController.cs
@@ -0,0 +1,34 @@
+﻿using Infrastructure.Data;
+using Core.Entities;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace API.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class ProductsController : ControllerBase
+    {
+        private readonly StoreContext _ctx;
+
+        public ProductsController(StoreContext context)
+        {
+            this._ctx = context;
+        }
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Product>>> GetProducts()
+        {
+            return Ok(await _ctx.Products.ToListAsync());
+        }
+
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Product>> GetProduct(int id)
+        {
+            return Ok(await _ctx.Products.FindAsync(id));
+        }
+    }
+}
